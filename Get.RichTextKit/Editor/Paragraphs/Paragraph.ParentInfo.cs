4137b8962238b2308f03e064d2717e95d82086fd
getdn <getdna99@gmail.com>
2023-07-24 23:49:34 +0700

    Parent API

---
+++ Get.RichTextKit/Editor/Paragraphs/Paragraph.ParentInfo.cs
@@ -0,0 +1,44 @@
+﻿using Get.RichTextKit.Editor.Paragraphs.Panel;
+using Get.RichTextKit.Editor.Structs;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Get.RichTextKit.Editor.Paragraphs;
+
+partial class Paragraph
+{
+    protected ParentInfo VirtualizedParentInfo
+    {
+        get
+        {
+
+            var parentInfo = ParentInfo;
+            if (parentInfo.Parent is null)
+            {
+                if (this is not IParagraphCollection paragraphCollection)
+                    throw new InvalidOperationException("The root paragraph must be a panel");
+                parentInfo = new(paragraphCollection, 0);
+            }
+            return parentInfo;
+        }
+    }
+    public ParentInfo ParentInfo { get; internal set; }
+    public ParagraphIndex GlobalParagraphIndex => new(GetParaIndex(null).ToArray());
+    IEnumerable<int> GetParaIndex(IParagraphCollection? stopAtParent)
+    {
+        var parentInfo = ParentInfo;
+        if (parentInfo.Parent is not Paragraph paragraph || paragraph == stopAtParent)
+        {
+            yield break;
+        }
+        else
+        {
+            foreach (var a in paragraph.GetParaIndex(stopAtParent))
+                yield return a;
+            yield return parentInfo.Index;
+        }
+    }
+}
